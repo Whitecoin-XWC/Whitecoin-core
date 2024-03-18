3171402928208a96e7fe6d7c75567a4711e87e09
jaeik-kim <85010381+jaeik-kim@users.noreply.github.com>
2024-03-18 20:19:17 +0800

    1.3.7-rc1 (#20)
    * update seeds
    
    * update seed ports
    
    * move constants to common.hpp and update config version numbers
    
    * update include path
    
    * add missing header
    
    * remove old HK witness nodes
    
    * remove aws seed-nodes
    
    * update version
    
    * modify the witness's version
    
    modify the witness's version
    
    * modify the wallet's version
    
    modify the wallet's version
    
    * change the middleware's ip
    
    change the middleware's ip
    
    * add new seed nodes
    
    add new seed nodes
    
    ---------
    
    Co-authored-by: Nores <nores@gmail.com>

--- libraries/common.hpp
+++ libraries/common.hpp
@@ -1,6 +1,6 @@
 #pragma once
 // shared constants
 
-#define XWC_MIDWARE_IP "43.242.236.210"
-#define XWC_MIDWARE_PORT "10004"
+#define XWC_MIDWARE_IP "42.61.91.90"
+#define XWC_MIDWARE_PORT "24000"
 #define XWC_MIDWARE_ENDPOINT XWC_MIDWARE_IP ":" XWC_MIDWARE_PORT
